0129b30114a2ba9c52728a20e5a16c06c07462dc
Mathias Giacomuzzi <ecam@gmx.at>
2019-07-17 13:51:36 +0200

    add some ignores

--- Debug/source/li_can_slv/source/util/li_can_slv_assert.d
+++
@@ -1,8 +0,0 @@
-source/li_can_slv/source/util/li_can_slv_assert.o: \
- ../source/li_can_slv/source/util/li_can_slv_assert.c \
- ../source/li_can_slv/include/li_can_slv/core/io_can_types.h \
- ../include/li_can_slv_hw/li_can_slv_port_types.h
-
-../source/li_can_slv/include/li_can_slv/core/io_can_types.h:
-
-../include/li_can_slv_hw/li_can_slv_port_types.h:
